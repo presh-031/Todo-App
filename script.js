bd9468fc8143e8849d832ab4bfc9ba6d11fd8844
presh-031 <akingbulugbe.dev@gmail.com>
2022-09-05 02:47:24 +0100

    about to start working on completed tasks-that inclides checkbox

--- script.js
+++ script.js
@@ -1,53 +1,66 @@
 "use strict";
 const newTaskInput = document.querySelector("#new-task");
+const allTasks = document.querySelector("ul");
 const itemCount = document.querySelector(".items-count");
+const bar = document.querySelector(".bar");
+const removeTask = document.querySelector(".delete-task");
+
 document.addEventListener("keydown", (e) => {
   if (e.key === "Enter") {
     e.preventDefault();
-    addNewTask();
-    showHiddenBar();
-    newTaskInput.value = "";
-    // deleteTask();
+    if (newTaskInput.value) {
+      addNewTask();
+      showBar();
+      newTaskInput.value = "";
+    }
   }
 });
 
-// const allTasksArr = [];
-const allTasks = document.querySelector("ul");
 function addNewTask() {
   const eachTask = document.createElement("li");
 
   eachTask.innerHTML =
-    "<input type='checkbox'> " +
+    "<input type='checkbox' class='checkbox'> " +
     `<p>${newTaskInput.value}</p>` +
     "<img class='delete-task' src='./images/icon-cross.svg'>";
-  // allTasksArr.push(eachTask);
-  // console.log(allTasksArr);
 
-  // allTasksArr.forEach((task) => {
-  // allTasks.appendChild(task);
   allTasks.append(eachTask);
   updateCount(1);
-  // });
-}
-function updateCount(num) {
-  itemCount.innerHTML = +itemCount.innerHTML + num;
 }
 
-function showHiddenBar() {
-  const bar = document.querySelector(".bar");
+function updateCount(num) {
+  itemCount.innerHTML = +itemCount.innerHTML + num;
+  if (itemCount.innerHTML) {
+    hideBar();
+  }
+}
+
+function hideBar() {
+  bar.classList.add("hidden");
+}
+
+function showBar() {
   bar.classList.remove("hidden");
 }
+
 // Delete tasks
 function deleteTask(task) {
   task.remove();
   updateCount(-1);
 }
-
 allTasks.addEventListener("click", (e) => {
+  // console.log(e.target.parentElement);
   if (e.target.classList.contains("delete-task")) {
     deleteTask(e.target.parentElement);
   }
 });
 
-// drag and drop
-sortable.create(ul);
+// Clear Tasks
+// const clearTask = document.querySelector(".checkbox");
+// clearTask.addEventListener("change", function () {
+//   if (this.checked) {
+//     console.log("Checkbox is checked..");
+//   } else {
+//     console.log("Checkbox is not checked..");
+//   }
+// });
