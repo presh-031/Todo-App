63abcbf7bef6166ef27982a23f9798633608738d
presh-031 <akingbulugbe.dev@gmail.com>
2022-09-28 15:15:51 +0100

    still working on color modes

--- todo.js
+++ todo.js
@@ -149,26 +149,27 @@ function filterBtnsColors() {
     });
   });
 }
+
 ///////////////////////////
 //////////////Dark Mode////
 ///////////////////////////
-// const btn = document.querySelector(".right-section");
-// const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+const btn = document.querySelector(".color-mode");
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
 
-// const currentTheme = localStorage.getItem("theme");
-// if (currentTheme == "dark") {
-//   document.body.classList.toggle("dark-theme");
-// } else if (currentTheme == "light") {
-//   document.body.classList.toggle("light-theme");
-// }
+const currentTheme = localStorage.getItem("theme");
+if (currentTheme == "dark") {
+  document.body.classList.toggle("dark-theme");
+} else if (currentTheme == "light") {
+  document.body.classList.toggle("light-theme");
+}
 
-// btn.addEventListener("click", function () {
-//   if (prefersDarkScheme.matches) {
-//     document.body.classList.toggle("light-theme");
-//     var theme = document.body.classList.contains("light-theme") ? "light" : "dark";
-//   } else {
-//     document.body.classList.toggle("dark-theme");
-//     var theme = document.body.classList.contains("dark-theme") ? "dark" : "light";
-//   }
-//   localStorage.setItem("theme", theme);
-// });
+btn.addEventListener("click", function () {
+  if (prefersDarkScheme.matches) {
+    document.body.classList.toggle("light-theme");
+    var theme = document.body.classList.contains("light-theme") ? "light" : "dark";
+  } else {
+    document.body.classList.toggle("dark-theme");
+    var theme = document.body.classList.contains("dark-theme") ? "dark" : "light";
+  }
+  localStorage.setItem("theme", theme);
+});
